367e97a71066340b413ca36c2f85b69404b1bdf6
uggrock <ugg.rock@gmail.com>
2015-12-26 19:40:21 +0100

    correcting path

--- perspectiveUI.js
+++ perspectiveUI.js
@@ -9,7 +9,7 @@ define(function(require, exports, module) {
 
   var TSCORE = require("tscore");
   var TSPOSTIO = require("tspostioapi");
-  var saveAs = require("libs/FileSaver.js/FileSaver.min");
+  var saveAs = require("libs/filesaver.js/FileSaver.min");
 
   var TMB_SIZES = ["100px", "200px", "300px", "400px", "500px"];
   var supportedFileTypeThumnailing = ['jpg', 'jpeg', 'png', 'gif'];
